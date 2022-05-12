a5cec6af8a423a146155b4dc543a5478d1f9a08f
harry <harry@dataclarityconsulting.co.uk>
2022-05-12 07:45:28 +0100

    All tests running

--- R/lmr/test-lmr.R
+++ R/lmr/test-lmr.R
@@ -1,109 +1,105 @@
-library(testthat)
-library(lmr)
+## Test that Pena-Yohai initial estimates are the same every time
+liver <- texmex::liver
 
-test_that("Pena-Yohai initial estimates are the same every time", {
-  liver <- texmex::liver
+for (run in 1:10){
+  i <- sample(1:nrow(liver), size = nrow(liver), replace = TRUE)
+  d <- liver[i, ]
 
-  for (run in 1:10){
-    i <- sample(1:nrow(liver), size = nrow(liver), replace = TRUE)
-    d <- liver[i, ]
+  m1 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
+           data = d)
+  m2 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
+           data = d)
 
-    m1 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
-             data = d)
-    m2 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
-             data = d)
+  expect_identical(m1, m2, info = "py produces the same output every time")
 
-    expect_identical(m1, m2, label = "py produces the same output every time")
+  m1 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
+           data = d, resid_keep_method = "proportion",
+           psc_keep = .75, resid_keep_prop = .75)
 
-    m1 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
-             data = d, resid_keep_method = "proportion",
-             psc_keep = .75, resid_keep_prop = .75)
+  expect_false(all(m1$coefficients == m2$coefficients),
+               info = "py produces different output with different settings")
 
-    expect_false(all(m1$coefficients == m2$coefficients),
-                 label = "py produces different output with different settings")
+  m2 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
+           data = d, resid_keep_method = "proportion",
+           psc_keep = .75, resid_keep_prop = .75)
 
-    m2 <- py(log(ALT.M) ~ log(ALT.B) + log(ALP.B) + log(AST.B) + as.numeric(dose),
-             data = d, resid_keep_method = "proportion",
-             psc_keep = .75, resid_keep_prop = .75)
+  expect_identical(m1, m2, info = "py produces the same output every time with user-supplied settings")
+}
 
-    expect_identical(m1, m2, label = "py produces the same output every time with user-supplied settings")
-  }
-})
+## Test that robustbase and MASS functions give the same result
+liver <- texmex::liver
 
-test_that("robustbase and MASS functions give the same result", {
-  liver <- texmex::liver
+set.seed(20210615)
+for (run in 1:10){
+  i <- sample(1:nrow(liver), size = nrow(liver), replace = TRUE)
+  d <- liver[i, ]
 
-  set.seed(20210615)
-  for (run in 1:10){
-    i <- sample(1:nrow(liver), size = nrow(liver), replace = TRUE)
-    d <- liver[i, ]
+  mrb <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
+             engine = "lmrob")
+  mr <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
+            engine = "rlm")
+  expect_equal(coef(mr), coef(mrb), tol = .002,
+               info = "lmrob and rlm give same coefficients")
+  expect_equal(mr$init$coefficients, mrb$init$coefficients, tol = 1e-7,
+               info = "lmrob and rlm use same initial coefficients")
+  expect_equal(mr$init$scale, mrb$init$scale, tol = 1e-7,
+               info = "lmrob and rlm use same initial scale")
+  expect_true(cor(resid(mrb), resid(mr)) > .999,
+              info = "lmrob and rlm produce the same residuals")
 
-    mrb <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
-               engine = "lmrob")
-    mr <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
+  mrr <- robust::lmRob(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
+                       control = robust::lmRob.control(efficiency = .85,
+                                                       weight = c("bisquare", "bisquare"),
+                                                       mxr = 200))
+  expect_equal(coef(mrr), coef(mrb), tol = .002,
+               info = "lmRob gives same coefficients")
+  expect_true(cor(resid(mrb), resid(mrr)) > .999,
+              info = "lmRob and lmr produce same residuals")
+}
+
+
+
+## Test that rfpe works correctly
+liver <- texmex::liver
+
+set.seed(20210615)
+for (i in 1:10){
+  i <- sample(1:nrow(liver), size = nrow(liver), replace = TRUE)
+  d <- liver[i, ]
+
+
+  mrb0 <- lmr(log(ALT.M) ~ 1, data = d,
+              engine = "lmrob")
+  mrb1 <- lmr(log(ALT.M) ~ log(ALT.B), data = d,
+              engine = "lmrob")
+  mrb2 <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
+              engine = "lmrob")
+
+  s <- mrb2$scale
+
+  expect_error(rfpe(mrb2),
+               info = "rfpe: failure if scale not provided")
+
+  expect_true(rfpe(mrb1, scale = s) < rfpe(mrb0, scale = s),
+              info = "rfpe: ok with lmrob, models 0 and 1")
+  expect_true(rfpe(mrb2, scale = s) < rfpe(mrb1, scale = s),
+              info = "rfpe: ok with lmrob, models 1 and 2")
+
+  mrb0 <- lmr(log(ALT.M) ~ 1, data = d,
               engine = "rlm")
-    expect_equal(coef(mr), coef(mrb), tol = .002,
-                 label = "lmrob and rlm give same coefficients")
-    expect_equal(mr$init$coefficients, mrb$init$coefficients, tol = 1e-7,
-                 label = "lmrob and rlm use same initial coefficients")
-    expect_equal(mr$init$scale, mrb$init$scale, tol = 1e-7,
-                 label = "lmrob and rlm use same initial scale")
-    expect_gt(cor(resid(mrb), resid(mr)), .999,
-              label = "lmrob and rlm produce the same residuals")
+  mrb1 <- lmr(log(ALT.M) ~ log(ALT.B), data = d,
+              engine = "rlm")
+  mrb2 <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
+              engine = "rlm")
 
-    mrr <- robust::lmRob(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
-                         control = robust::lmRob.control(efficiency = .85,
-                                                         weight = c("bisquare", "bisquare"),
-                                                         mxr = 200))
-    expect_equal(coef(mrr), coef(mrb), tol = .002,
-                 label = "lmRob gives same coefficients")
-    expect_gt(cor(resid(mrb), resid(mrr)), .999,
-              label = "lmRob and lmr produce same residuals")
-  }
-})
+  s <- mrb2$scale
 
+  expect_true(rfpe(mrb1, scale = s) < rfpe(mrb0, scale = s),
+              info = "rfpe: ok with rlm, models 0 and 1")
+  expect_true(rfpe(mrb2, scale = s) < rfpe(mrb1, scale = s),
+              info = "rfpe: ok with rlm, models 1 and 2")
+}
 
-test_that("rfpe works correctly", {
-  liver <- texmex::liver
-
-  set.seed(20210615)
-  for (i in 1:10){
-    i <- sample(1:nrow(liver), size = nrow(liver), replace = TRUE)
-    d <- liver[i, ]
-
-
-    mrb0 <- lmr(log(ALT.M) ~ 1, data = d,
-                engine = "lmrob")
-    mrb1 <- lmr(log(ALT.M) ~ log(ALT.B), data = d,
-               engine = "lmrob")
-    mrb2 <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
-                engine = "lmrob")
-
-    s <- mrb2$scale
-
-    expect_error(rfpe(mrb2),
-                   label = "rfpe: failure if scale not provided")
-
-    expect_lt(rfpe(mrb1, scale = s), rfpe(mrb0, scale = s),
-              label = "rfpe: ok with lmrob, models 0 and 1")
-    expect_lt(rfpe(mrb2, scale = s), rfpe(mrb1, scale = s),
-              label = "rfpe: ok with lmrob, models 1 and 2")
-
-    mrb0 <- lmr(log(ALT.M) ~ 1, data = d,
-                engine = "rlm")
-    mrb1 <- lmr(log(ALT.M) ~ log(ALT.B), data = d,
-                engine = "rlm")
-    mrb2 <- lmr(log(ALT.M) ~ log(ALT.B) + as.numeric(dose), data = d,
-                engine = "rlm")
-
-    s <- mrb2$scale
-
-    expect_lt(rfpe(mrb1, scale = s), rfpe(mrb0, scale = s),
-              label = "rfpe: ok with rlm, models 0 and 1")
-    expect_lt(rfpe(mrb2, scale = s), rfpe(mrb1, scale = s),
-              label = "rfpe: ok with rlm, models 1 and 2")
-  }
-})
 
 
 
--- R/margarita/test-helpers.R
+++ R/margarita/test-helpers.R
@@ -1,98 +1,94 @@
-context("margarita helpers")
 
-test_that("margaritaScale traps erroneous input and returns correct substring", {
-  margaritaScale <- margarita:::margaritaScale
-  expect_that(margaritaScale("robots"), matches("r"))
-  expect_that(margaritaScale("pingpong"), matches("p"))
-  expect_that(margaritaScale("dumbdumb"), matches("d"))
-  expect_that(margaritaScale(2), throws_error())
-  expect_that(margaritaScale(letters), throws_error())
-  f <- function(x){
-    res <- try(margaritaScale(x), silent=TRUE)
-    if (class(res) == "try-error"){ FALSE }
-    else { TRUE }
-  }
-  s <- sapply(letters, f)
-  for (i in 1:sum(s)) # s is a logical vector
-    expect_that(letters[s][i], matches(c("d", "p", "r")[i]))
-})
+## Test margaritaScale traps erroneous input and returns correct substring
+margaritaScale <- margarita:::margaritaScale
+expect_equal(margaritaScale("robots"), "r")
+expect_equal(margaritaScale("pingpong"), "p")
+expect_equal(margaritaScale("dumbdumb"), "d")
+expect_error(margaritaScale(2))
+expect_error(margaritaScale(letters))
 
-test_that("getCIquantiles behaves as expected", {
-  getCIquantiles <- margarita:::getCIquantiles
-  expect_that(getCIquantiles(.05), equals(c(0.025, 0.50, 0.975)))
-  expect_that(getCIquantiles(.1), equals(c(0.050, 0.50, 0.950)))
-  expect_that(getCIquantiles(c(.1, .5)), equals(c(.050, .250, .50, .750, .950)))
-  expect_that(getCIquantiles(c(.5, .1)), equals(sort(getCIquantiles(c(.5, .1)))))
-  expect_that(getCIquantiles(0), throws_error())
-  expect_that(getCIquantiles(1), throws_error())
-  expect_that(getCIquantiles(c(.05, .1, .5)), throws_error())
-})
-
-test_that("Segments are returned correctly", {
-  getSegmentData <- margarita:::getSegmentData
-  data <- data.frame(rmvnorm(100, rep(0, 3)))
-
-  expect_that(getSegmentData(data), throws_error())
-
-  data$groups <- rep(LETTERS[1:2], each=50)
-
-  expect_that(getSegmentsData(data), throws_error())
-  
-  data[, 1:3] <- t(apply(data[, 1:3], 1, sort))
-
-  expect_that(getSegmentData(data)[[1]], is_equivalent_to(data[, c("X1", "X3", "groups")]))
-  expect_that(getSegmentData(data)[[2]], equals(NULL))
-
-  data <- data.frame(rmvnorm(100, rep(0, 7)))
-  data$groups <- rep(LETTERS[1:2], each=50)
-  data[, 1:7] <- t(apply(data[, 1:7], 1, sort))
-
-  expect_that(getSegmentData(data)[[1]], is_equivalent_to(data[, c("X1", "X5", "groups")]))
-  expect_that(getSegmentData(data)[[2]], is_equivalent_to(data[, c("X2", "X4", "groups")]))
-
-  data <- data.frame(rmvnorm(100, rep(0, 2)))
-  data$groups <- rep(LETTERS[1:2], each=50)
-  data[, 1:2] <- t(apply(data[, 1:2], 1, sort))
-
-  expect_that(getSegmentData(data), throws_error())
-})
-
-test_that("summary.margarita arithmetic works", {
-  # Implicity tests as.data.frame.summary.margarita, too
-  liver$ndose <- as.numeric(liver$dose)
-  mm <- lmr(log(ALT.M) ~ log(ALT.B) + ndose, data=liver)
-  liver$r <- resid(mm)
-  em <- evm(r, data=liver, qu=.5, xi=~ndose, method="sim", iter=10500, verbose=FALSE)
-  mar <- margarita(mm, em, newdata=data.frame(ndose=1:4), baseline="ALT.B")
+f <- function(x){
+  res <- try(margaritaScale(x), silent=TRUE)
+  if (class(res) == "try-error"){ FALSE }
+  else { TRUE }
+}
+s <- sapply(letters, f)
+for (i in 1:sum(s)) # s is a logical vector
+  expect_equal(letters[s][i], c("d", "p", "r")[i])
 
 
-  rl <- simulate(mar, M=c(100, 500, 1000))
-  srl <- summary(rl)
-
-  # division
-  d <- srl / 25
-  whd <- lapply(srl, function(x) x/25)
-  whd <- do.call("rbind", whd)
-  expect_that(as.data.frame(whd), is_equivalent_to(as.data.frame(d)[, 1:5]))
-
-  # multiplication
-  d <- srl * 25
-  whd <- lapply(srl, function(x) x*25)
-  whd <- do.call("rbind", whd)
-  expect_that(as.data.frame(whd), is_equivalent_to(as.data.frame(d)[, 1:5]))
-
-  # subtraction
-  d <- srl - 25
-  whd <- lapply(srl, function(x) x-25)
-  whd <- do.call("rbind", whd)
-  expect_that(as.data.frame(whd), is_equivalent_to(as.data.frame(d)[, 1:5]))
-  
-  # addition
-  d <- srl + 25
-  whd <- lapply(srl, function(x) x+25)
-  whd <- do.call("rbind", whd)
-  expect_that(as.data.frame(whd), is_equivalent_to(as.data.frame(d)[, 1:5]))
-  })
+## Test getCIquantiles behaves as expected
+getCIquantiles <- margarita:::getCIquantiles
+expect_equal(getCIquantiles(.05), c(0.025, 0.50, 0.975))
+expect_equal(getCIquantiles(.1), c(0.050, 0.50, 0.950))
+expect_equal(getCIquantiles(c(.1, .5)), c(.050, .250, .50, .750, .950))
+expect_equal(getCIquantiles(c(.5, .1)), sort(getCIquantiles(c(.5, .1))))
+expect_error(getCIquantiles(0))
+expect_error(getCIquantiles(1))
+expect_error(getCIquantiles(c(.05, .1, .5)))
 
 
+## Test segments are returned correctly
+getSegmentData <- margarita:::getSegmentData
+data <- data.frame(rmvnorm(100, rep(0, 3)))
 
+expect_error(getSegmentData(data))
+
+data$groups <- rep(LETTERS[1:2], each=50)
+
+expect_error(getSegmentsData(data))
+
+data[, 1:3] <- t(apply(data[, 1:3], 1, sort))
+
+expect_equivalent(getSegmentData(data)[[1]], data[, c("X1", "X3", "groups")])
+expect_equal(getSegmentData(data)[[2]], NULL)
+
+data <- data.frame(rmvnorm(100, rep(0, 7)))
+data$groups <- rep(LETTERS[1:2], each=50)
+data[, 1:7] <- t(apply(data[, 1:7], 1, sort))
+
+expect_equivalent(getSegmentData(data)[[1]], data[, c("X1", "X5", "groups")])
+expect_equivalent(getSegmentData(data)[[2]], data[, c("X2", "X4", "groups")])
+
+data <- data.frame(rmvnorm(100, rep(0, 2)))
+data$groups <- rep(LETTERS[1:2], each=50)
+data[, 1:2] <- t(apply(data[, 1:2], 1, sort))
+
+expect_error(getSegmentData(data))
+
+
+## Test summary.margarita arithmetic works
+# Implicity tests as.data.frame.summary.margarita, too
+liver$ndose <- as.numeric(liver$dose)
+mm <- lmr(log(ALT.M) ~ log(ALT.B) + ndose, data=liver)
+liver$r <- resid(mm)
+em <- evm(r, data=liver, qu=.5, xi=~ndose, method="sim", iter=10500, verbose=FALSE)
+mar <- margarita(mm, em, newdata=data.frame(ndose=1:4), baseline="ALT.B", arm = "ndose")
+
+
+rl <- simulate(mar, M=c(100, 500, 1000))
+srl <- summary(rl)
+
+# division
+d <- srl / 25
+whd <- lapply(srl, function(x) x/25)
+whd <- do.call("rbind", whd)
+expect_equivalent(as.data.frame(whd), as.data.frame(d)[, 1:5])
+
+# multiplication
+d <- srl * 25
+whd <- lapply(srl, function(x) x*25)
+whd <- do.call("rbind", whd)
+expect_equivalent(as.data.frame(whd), as.data.frame(d)[, 1:5])
+
+# subtraction
+d <- srl - 25
+whd <- lapply(srl, function(x) x-25)
+whd <- do.call("rbind", whd)
+expect_equivalent(as.data.frame(whd), as.data.frame(d)[, 1:5])
+
+# addition
+d <- srl + 25
+whd <- lapply(srl, function(x) x+25)
+whd <- do.call("rbind", whd)
+expect_equivalent(as.data.frame(whd), as.data.frame(d)[, 1:5])
